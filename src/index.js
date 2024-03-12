0deabd53db4f8594036b9281ab335cc0c1d348c6
KasaganiGanesh <ganeshkasagani@lyrostech.com>
2024-03-12 13:48:10 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App13a from "./App13a";
+import App18 from "./App18"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App13a />
+    <App18 />
   </>
 );
